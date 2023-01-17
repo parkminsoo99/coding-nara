f556588f74a1a912a6bb556394d612c4f4e3d51b
parkminsoo99 <zzangorc99@naver.com>
2023-01-18 03:21:30 +0900

    결제완료시 정보 디비저장

--- lib_login/template.js
+++ lib_login/template.js
@@ -3,18 +3,87 @@ module.exports = {
       return `
       <!doctype html>
       <html>
-      <head>
+      <head>    
         <title>Login TEST - ${title}</title>
         <meta charset="utf-8">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" ></script>
-        <!-- iamport.payment.js -->
-        <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
         <style>
+          @import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css);
+  
           body {
               font-family: 'Noto Sans KR', sans-serif;
               background-color: white;
               margin: 50px;
+  
           }
+          
+          .number-circle-list {
+            list-style: none;
+            padding-left: 1rem;
+            counter-reset: circle-counter;
+          }
+          
+          .number-circle-list--list-item {
+            counter-increment: circle-counter;
+            margin-bottom: 0.25rem;
+          }
+          .number-circle-list--list-item:before {
+            content: counter(circle-counter);
+            background-color: #e1e1e1;
+            width: 1.5rem;
+            height: 1.5rem;
+            border-radius: 50%;
+            display: inline-block;
+            font-size: 0.75rem;
+            line-height: 1.5rem;
+            color: black;
+            text-align: center;
+            margin-right: 0.5rem;
+            position: relative;
+            top: -2px;
+          }
+          .number-circle-list--list-item:last-child {
+            margin-bottom: 0;
+          }
+          .number-circle-list--list-item .number-circle-list--list-item {
+            margin-left: 0.25rem;
+          }
+          
+          .number-circle-list--primary-color .number-circle-list--list-item:before {
+            background-color: #0d5cab;
+            color: white;
+          }
+          
+          .number-circle-list--secondary-color .number-circle-list--list-item:before {
+            background-color: #72c267;
+            color: white;
+          }
+          
+          .number-circle-list--tertiary-color .number-circle-list--list-item:before {
+            background-color: #168dde;
+            color: white;
+          }
+          
+          html {
+            font-size: 16px;
+          }
+          
+          
+          
+          
+          
+          
+          .container{
+            width: 90%;
+            height: 100%;
+            overflow: hidden;
+          }
+          .container img{
+            max-width: 100%;
+            height: auto;
+            display: block;
+
+          }
+         
           
           header{
             margin-top: 80px;
@@ -48,7 +117,18 @@ module.exports = {
             padding: 15px;
             cursor: pointer;
           }
+          
           .nav-right a{
+            text-align: right;
+            text-decoration: none;
+            color: black;
+          }
+          .nav-left{
+            padding: 15px;
+            cursor: pointer;
+          }
+          
+          .nav-left a{
             text-align: right;
             text-decoration: none;
             color: black;
@@ -101,7 +181,6 @@ module.exports = {
           .btn:hover {
               background-color: #595787;
           }
-<<<<<<< HEAD
           .buttons-container {
             width: 100%;
             height: 100vh;
@@ -211,13 +290,165 @@ module.exports = {
           #mg{
             margin: 120px;
           }
-          .
+          .col{
+            display : flex;
+          }
+          .col_title{
+            margin : 10px 30px 10px 30px;
+          }
+          .btn {
+            background: hsl(var(--hue), 98%, 80%);
+            border: none;
+            border-radius: 7px;
+            cursor: pointer;
+            color: black;
+            font: 600 1.05rem/1 "Nunito", sans-serif;
+            letter-spacing: 0.05em;
+            overflow: hidden;
+            padding: 1.15em 3.5em;
+            min-height: 3.3em;
+            position: relative;
+            text-transform: lowercase;
+          }
+          .btn--yellow {
+            --hue: 46;
+          }
+          .btn--green {
+            --hue: 163;
+          }
+          .btn--purple {
+            --hue: 244;
+          }
+          .btn--red {
+            --hue: 0;
+          }
+          .btn--blue {
+            --hue: 210;
+          }
+          .btn:active, .btn:focus {
+            outline: 3px solid hsl(calc(var(--hue) + 90), 98%, 80%);
+          }
+          .btn + .btn {
+            margin-top: 2.5em;
+          }
+          .btn__txt {
+            position: relative;
+            z-index: 2;
+          }
+          .btn__bg {
+            background: hsl(var(--hueBg), 98%, 80%);
+            border-radius: 50%;
+            display: block;
+            height: 0;
+            left: 50%;
+            margin: -50% 0 0 -50%;
+            padding-top: 100%;
+            position: absolute;
+            top: 50%;
+            width: 100%;
+            transform: scale(0);
+            transform-origin: 50% 50%;
+            transition: transform 0.175s cubic-bezier(0.5, 1, 0.89, 1);
+            z-index: 1;
+          }
+          .btn__bg:nth-of-type(1) {
+            --hueBg: calc(var(--hue) - 90);
+            transition-delay: 0.1725s;
+          }
+          .btn__bg:nth-of-type(2) {
+            --hueBg: calc(var(--hue) - 180);
+            transition-delay: 0.115s;
+          }
+          .btn__bg:nth-of-type(3) {
+            --hueBg: calc(var(--hue) - 270);
+            transition-delay: 0.0575s;
+          }
+          .btn__bg:nth-of-type(4) {
+            --hueBg: calc(var(--hue) - 360);
+            transition-delay: 0s;
+          }
+          .btn:hover .btn__bg, .btn:focus .btn__bg, .btn:active .btn__bg {
+            transform: scale(1.5);
+            transition: transform 0.35s cubic-bezier(0.11, 0, 0.5, 0);
+          }
+          .btn:hover .btn__bg:nth-of-type(1), .btn:focus .btn__bg:nth-of-type(1), .btn:active .btn__bg:nth-of-type(1) {
+            transition-delay: 0.115s;
+          }
+          .btn:hover .btn__bg:nth-of-type(2), .btn:focus .btn__bg:nth-of-type(2), .btn:active .btn__bg:nth-of-type(2) {
+            transition-delay: 0.23s;
+          }
+          .btn:hover .btn__bg:nth-of-type(3), .btn:focus .btn__bg:nth-of-type(3), .btn:active .btn__bg:nth-of-type(3) {
+            transition-delay: 0.345s;
+          }
+          .card {
+            background: #fff;
+            border-radius: 2px;
+            display: inline-block;
+            height: 300px;
+            margin: 1rem;
+            position: relative;
+            width: 300px;
+          }
+          
+        
+          
+          .card-3 {
+            box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+          }
+          .ask{
+            display: inline-block;
+          }
+          .ask_title{
+            color: #333;
+            border-bottom: 4px solid #7FFF65;
+            padding-bottom: 5px;
+            position: relative;
+          }
+          
+          .ask_title:before{
+          /*     content: '';
+              position: absolute;
+              bottom: -20px;
+              left: 50%;
+              width: 20px;
+              height: 20px;
+              background: white;
+              border-left: 4px solid #7FFF65;
+             */
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+            left: 30px;
+            bottom: -30px;
+            border: 15px solid;
+            border-color: #7FFF65 transparent transparent #7FFF65;
+          }
+          
+          .ask_title:after{
+          /*     content: '';
+              position: absolute;
+              bottom: -20px;
+              left: 49%;
+              width: 15px;
+              height: 31px;
+              transform: rotate(51deg);
+              border-right: 4px solid #7FFF65; */
+              
+            content: ' ';
+            position: absolute;
+            width: 0;
+            height: 0;
+            left: 34px;
+            bottom: -20px;
+            border: 15px solid;
+            border-color: #fff transparent transparent #fff;
+          }
+          
 
           
-=======
-          .topNavIn {position:relative; width:1080px; margin:0 auto;}
->>>>>>> 0f35e68d (수강신청에 필요한 UI제작 및 디비에서 강사 정보+강의 시간등의 필요한 정보를 가져옴)
       </style>
+      
       <nav>
       <img src='http://localhost:3000/image/1.png'
       alt="카카오 라이언" width="125" height="125" align="left" border="0">
@@ -233,148 +464,11 @@ module.exports = {
       </nav>
       </head>
       <body>
+ 
           ${authStatusUI}
           ${body}
-         
-        </div>
       </body>
       </html>
       `;
-    },ASK : function(title, body, authStatusUI){
-        return `
-        <!doctype html>
-        <html>
-        <head>
-          <title>Login TEST - ${title}</title>
-          <meta charset="utf-8">
-          <style>
-            body {
-                font-family: 'Noto Sans KR', sans-serif;
-                background-color: white;
-                margin: 50px;
-            }
-            header{
-              margin-top: 80px;
-            }
-            img{
-              position: absolute;
-              left: 0px;
-              top: 0px
-            }
-            .nav-container{
-              display: flex;
-              position: fixed;
-              top : 0;
-              flex-direction: row;
-              width: 100%;
-              margin: 0;
-              padding: 0;
-              background-color: white;
-              list-style-type: none;
-            }
-            .nav-item{
-              padding: 15px;
-              cursor: pointer;
-            }
-            .nav-item a{
-              text-align: center;
-              text-decoration: none;
-              color: black;
-            }
-            .nav-right{
-              padding: 15px;
-              cursor: pointer;
-            }
-            .nav-right a{
-              text-align: right;
-              text-decoration: none;
-              color: black;
-            }
-            .background {
-                background-color: white;
-                height: auto;
-                width: 90%;
-                max-width: 450px;
-                padding: 10px;
-                margin: 0 auto;
-                border-radius: 5px;
-                box-shadow: 0px 40px 30px -20px rgba(0, 0, 0, 0.3);
-                text-align: center;
-            }
-    
-            form {
-                display: flex;
-                padding: 30px;
-                flex-direction: column;
-            }
-    
-            .login {
-                border: none;
-                border-bottom: 2px solid #D1D1D4;
-                background: none;
-                padding: 10px;
-                font-weight: 700;
-                transition: .2s;
-                width: 75%;
-            }
-            .login:active,
-            .login:focus,
-            .login:hover {
-                outline: none;
-                border-bottom-color: #6A679E;
-            }
-    
-            .btn {            
-                border: none;
-                width: 75%;
-                background-color: #6A679E;
-                color: white;
-                padding: 15px 0;
-                font-weight: 600;
-                border-radius: 5px;
-                cursor: pointer;
-                transition: .2s;
-            }
-            .btn:hover {
-                background-color: #595787;
-            }
-            .topNavIn {position:relative; width:1080px; margin:0 auto;}
-            .aside_class{
-              width: 200px;
-              height: 890px;
-            }
-            .menu_item{
-              width: 200px;
-              height: 56px;
-            }
-            .question{
-              width:100%;
-              height:890px;
-            }
-            .question_li_a::before{
-              content : "Q  "
-            }
-        </style>
-        <nav>
-        <img src="http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg"
-        alt="카카오 라이언" width="200" height="100" align="left" border="0">
-          <div class="background">
-            <ul class="nav-container">
-              <li class="nav-item"><a href="/">코딩나라란</a><li>
-              <li class="nav-item"><a href="/curriculum">커리큘럼</a><li>
-              <li class="nav-item"><a href="/enroll/sub">수강신청</a><li>
-              <li class="nav-item"><a href="/review">강좌후기</a><li>
-              <li class="nav-item"><a href="/ask">고객센터</a><li>
-            </ul>
-        </nav>
-        </head>
-        <body>
-            ${authStatusUI}
-            ${body}
-           
-          </div>
-        </body>
-        </html>
-        `;
     }
   }
