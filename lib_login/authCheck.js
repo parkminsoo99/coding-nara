739c75d028f355f8a0809aeca97da49b3a72a296
urinaner <urinaner@naver.com>
2023-02-01 01:47:19 +0900

    웹프론트_2023_2_1일 일부 완성

--- lib_login/authCheck.js
+++ lib_login/authCheck.js
@@ -7,8 +7,7 @@ module.exports = {
       }
     },
     statusUI: function (request, response) {
-      var authStatusUI = `<div align="right"><a href="/auth/login"
-      >로그인</a></div>`;
+      var authStatusUI = '<div align="right"><a href="/auth/login">로그인</a></div>';
       if (this.isOwner(request, response)) {
         authStatusUI = `<div align="right">${request.session.nickname}님 환영합니다 
         <a href="/auth/logout"
@@ -36,12 +35,4 @@ module.exports = {
       }
       return authStatusUI;
     }
-    // ,
-    // Payment_Username: function (request, response) {
-    //   if (this.isOwner(request, response)) {
-    //     console.log('r_s_e',request.session.email);
-    //     authStatusUI = request.session.email;
-    //   }
-    //   return authStatusUI;
-    // }
   }
