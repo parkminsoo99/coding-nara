f1d38f7e1e5f041e4b5b457565b568516127f3b5
parkminsoo99 <zzangorc99@naver.com>
2023-02-17 18:20:58 +0900

    환불페이지 구현 완료

--- js/mypage.js
+++ js/mypage.js
@@ -15,14 +15,36 @@ let mypage_list = {
             }
         })
     },
-    refund_button : function() {
-        $.ajax({
-            type: "post", 
-            url:"/myinfo/refund_page",
-            dataType: "json",
-            data: {
-                CEA: cerNum
-            }
-        })
-    }
+    refund_button : function(number) {
+        var item = document.querySelector('button[id=refund_button'+number+']');
+        var subject = item.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild.textContent;
+        var str = item.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild.textContent;
+        var count = item.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.firstElementChild.textContent;
+        var teacher = item.parentElement.previousElementSibling.previousElementSibling.firstElementChild.textContent;
+        var str_split = str.split(' ');
+        var time = str_split[0];
+        var day = str_split[1];
+        document.location.href="/myinfo/refund_page?Subject="+subject+"&Time=" + time + "&Day=" + day + "&Teacher=" + teacher;
+    },
+    copy :function copy(number){
+          // div 내부 텍스트 취득
+          const valOfDIV = document.getElementById("div"+number).innerText;
+  
+          // textarea 생성
+          const textArea = document.createElement('textarea');
+  
+          // textarea 추가
+          document.body.appendChild(textArea);
+          
+          // textara의 value값으로 div내부 텍스트값 설정
+          textArea.value = valOfDIV;
+  
+          // textarea 선택 및 복사
+          textArea.select();
+          document.execCommand('copy');
+  
+          // textarea 제거
+          document.body.removeChild(textArea);
+          alert('강의 코드가 복사되었습니다.');
+    },
 }
